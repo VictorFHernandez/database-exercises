71471912aa3adcf5f1f12f6c303a205c76d00fea
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-18 12:32:21 -0500

    created the albums TABLE

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,14 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (75) NOT NULL,
+    name_of_record VARCHAR (100) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
