ae0b53ef4f0c5c79d72b03aef820c59770ce4649
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-18 13:49:09 -0500

    finished step 5

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,9 +6,13 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR (75) NOT NULL,
     name_of_record VARCHAR (100) NOT NULL,
-    release_date INT NOT NULL,
+    release_date DATE NOT NULL,
     sales FLOAT NOT NULL,
     genre VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 );
 
+/* completed step 5: Open a terminal,
+   and run the script as codeup_test_user with the following command:
+   mysql -u codeup_test_user -p < albums_migration.sql;
+ */
