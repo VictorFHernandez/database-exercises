3a32e16f69c5892c65412064257d3e454c189250
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-19 11:10:32 -0500

    finished exercise 3

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,9 +1,17 @@
 USE codeup_test_db;
 
 SELECT name_of_record FROM albums;
+UPDATE albums
+SET sales = sales * 10;
 
 SELECT name_of_record FROM albums
 WHERE release_date < 1980;
+UPDATE albums
+SET release_date = 1800;
+WHERE release_date < 1980;
 
 SELECT name_of_record FROM albums
+WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
