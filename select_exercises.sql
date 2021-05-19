f4854e84546f15f1b65bf08b50fd66a355347e21
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-19 10:39:32 -0500

    finished step 3 of exercise 2

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,3 +8,16 @@ SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
 SELECT release_date FROM albums
 WHERE name_of_record = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
+SELECT 'The genre for Nevermind' AS 'Info';
+SELECT genre FROM albums
+WHERE name_of_record = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'Info';
+
+
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
+
+
+
+SELECT 'All the albums with a genre of "Rock"' AS 'Info';
