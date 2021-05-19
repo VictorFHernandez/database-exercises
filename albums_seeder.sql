126ffdae5a757365c424e3eea9b5cc33f96173b8
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-19 10:11:19 -0500

    finished exercise 1

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,6 +1,84 @@
 USE codeup_test_db;
 
 INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, Progressive rock', 21.5);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'R&B, Soul, Pop, Soundtrack', 28.4);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, Soft rock, Folk rock', 32.2);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 21.6);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Various artists', 'Grease: The Original Soundtrack from the Motion Picture', 1978, 'Soundtrack', 14.4);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock, Heavy metal, Folk', 29.0);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Bad', 1987, 'Pop, Funk, Rock', 19.3);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Alanis Morissette', 'Jagged Little Pill', 1995, 'Alternative rock', 24.4);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Shania Twain', 'Come On Over', 1997, 'Country, Pop', 29.6);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Celine Dion', 'Falling into You', 1996, 'Pop, Soft rock', 20.2);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 'Rock', 13.1);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Eagles', 'Hotel California', 1976, 'Rock, Soft rock, Folk rock', 21.5);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Various artists', 'Dirty Dancing', 1987, 'Pop, Rock, R&B', 17.9);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Adele', '21', 2011, 'Pop, Soul', 25.3);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Celine Dion', 'Let''s Talk About Love', 1997, 'Pop, Soft rock', 19.3);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('The Beatles', '1', 2000, 'Rock', 22.6);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Dangerous', 1991, 'Rock, Funk, Pop', 16.3);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Madonna', 'The Immaculate Collection', 1990, 'Pop, Dance', 19.4);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Kid Cudi', 'Man on the Moon II: The Legend of Mr. Rager', 2009, 'Hip-hop', 23.7);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Drake', 'Take Care', 2011, 'Hip-hop', 37.2);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
+VALUES ('Bad Bunny', 'YHLQMDLG', 2020, 'Hip-hop, Latin, Reggaeton', 25.8);
+
+INSERT INTO albums (artist, name_of_record, release_date, genre, sales)
 VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1),
        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2),
@@ -23,4 +101,7 @@ VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
        ('The Beatles', '1', 2000, 'Rock', 22.6),
        ('Michael Jackson', 'Dangerous', 1991, 'Rock, Funk, Pop', 16.3),
        ('Madonna', 'The Immaculate Collection', 1990, 'Pop, Dance', 19.4),
-       ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4);
+       ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4),
+       ('Kid Cudi', 'Man on the Moon II: The Legend of Mr. Rager', 2009, 'Hip-hop', 23.7),
+       ('Drake', 'Take Care', 2011, 'Hip-hop', 37.2),
+       ('Bad Bunny', 'YHLQMDLG', 2020, 'Hip-hop, Latin, Reggaeton', 25.8);
