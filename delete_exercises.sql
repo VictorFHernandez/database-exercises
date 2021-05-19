797da05fbf93741276511d3c6486b99b4eb30f19
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-19 11:55:22 -0500

    finished basic Statements exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,9 +3,17 @@ USE codeup_test_db;
 SELECT name_of_record FROM albums
 WHERE release_date > 1991;
 
+DELETE FROM albums
+WHERE release_date > 1991;
 
 SELECT name_of_record FROM albums;
-WHERE genre = 'Disco'
+WHERE genre = 'Disco';
+
+DELETE FROM albums
+WHERE genre = 'Disco';
 
 SELECT name_of_record FROM albums;
+WHERE artist = 'Adele';
+
+DELETE FROM albums
 WHERE artist = 'Adele';
