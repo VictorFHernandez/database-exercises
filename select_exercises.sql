72ec66a4ad110c4deb9d3f5b88f5e6e59a048590
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-19 10:43:39 -0500

    finished step 4 from exercise 2

--- select_exercises.sql
+++ select_exercises.sql
@@ -12,9 +12,9 @@ SELECT 'The genre for Nevermind' AS 'Info';
 SELECT genre FROM albums
 WHERE name_of_record = 'Nevermind';
 
-SELECT 'Which albums were released in the 1990s' AS 'Info';
-
-
+SELECT 'albums released in the 1990s' AS 'Info';
+SELECT name_of_record FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
 
 SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
 
