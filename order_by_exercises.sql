54598143b48fec5199caa1c6ebaae0f83c96bfa4
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-20 13:56:34 -0500

    finished step 2 of the order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,8 @@
 USE employees;
 
 SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
 
 SELECT * FROM employees
 WHERE first_name = 'Irena'
