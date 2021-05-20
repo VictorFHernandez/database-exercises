c4348553840bfae969496997cc5bcf7c6337d277
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-20 15:44:56 -0500

    finished step 2 of the FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,43 +1,43 @@
 USE employees;
+--
+-- SELECT * FROM employees
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+-- ORDER BY first_name;
+--
+-- SELECT * FROM employees
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+-- ORDER BY first_name DESC, last_name DESC;
+--
+-- SELECT * FROM employees
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+-- ORDER BY last_name, first_name;
+--
+-- SELECT * FROM employees
+-- WHERE first_name = 'Irena'
+--    OR first_name = 'Vidya'
+--    OR first_name = 'Maya';
+--
+-- SELECT * FROM employees
+-- WHERE gender = 'Male'
+--     AND first_name = 'Irena'
+--    OR first_name = 'Vidya'
+--    OR first_name = 'Maya'
+--
+-- SELECT * FROM employees
+-- WHERE last_name LIKE 'E%';
+-- ORDER BY emp_no DESC;
+--
+-- SELECT * FROM employees
+-- WHERE last_name LIKE 'E%'
+--    OR last_name LIKE '%E';
 
-SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name;
-
-SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name DESC, last_name DESC;
-
-SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, first_name;
-
-SELECT * FROM employees
-WHERE first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya';
-
-SELECT * FROM employees
-WHERE gender = 'Male'
-    AND first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya'
-
-SELECT * FROM employees
-WHERE last_name LIKE 'E%';
-ORDER BY emp_no DESC;
-
-SELECT * FROM employees
-WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%E';
-
-SELECT * FROM employees
+SELECT CONCAT(first_name, ' ', last_name) FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E';
 
-SELECT * FROM employees
-WHERE last_name LIKE '%q%';
+-- SELECT * FROM employees
+-- WHERE last_name LIKE '%q%';
 
-SELECT * FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+-- SELECT * FROM employees
+-- WHERE last_name LIKE '%q%'
+--   AND last_name NOT LIKE '%qu%';
