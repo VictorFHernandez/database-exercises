ad3d1d52e5b03c7841706df5e5bd89b71be5dbea
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-20 13:42:08 -0500

    finished step 1 of exercise 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,6 +4,12 @@ SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT * FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+
+SELECT * FROM employees
 WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees
