c2d24e1eed956ca413d9e3b45011803ed3f3e540
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 09:39:35 -0500

    finished step 4 from FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -31,7 +31,8 @@ USE employees;
 -- WHERE last_name LIKE 'E%'
 --    OR last_name LIKE '%E';
 
--- SELECT CONCAT(first_name, ' ', last_name) FROM employees
+-- SELECT CONCAT(first_name, ' ', last_name)
+-- AS 'first and last names' FROM employees
 -- WHERE last_name LIKE 'E%'
 --   AND last_name LIKE '%E';
 
@@ -43,5 +44,6 @@ USE employees;
 --   AND last_name NOT LIKE '%qu%';
 
 SELECT * FROM employees
-WHERE MONTH (birth_date) = 12
+WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
+AND MONTH (birth_date) = 12
 AND DAY(birth_date) = 25;
