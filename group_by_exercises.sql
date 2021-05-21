8e00188206c10ce861dedd7c10f49e3a9501069d
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 12:19:44 -0500

    updated GROUP BY exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,12 +12,12 @@ WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E'
 GROUP BY first_name, last_name ORDER BY first_name, last_name ASC;
 
-SELECT COUNT(last_name), last_name FROM employees
+SELECT last_name, COUNT(last_name) FROM employees
 WHERE last_name Like '%q%'
 AND last_name NOT LIKE '%qu%'
 GROUP BY last_name ORDER BY last_name;
 
 
-SELECT COUNT(gender), gender FROM employees
+SELECT gender, COUNT(gender) FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender;
