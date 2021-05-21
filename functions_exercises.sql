027c89839777e95a13d8a69d66ee46a830ab6e05
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 10:17:30 -0500

    finished FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,9 +1,9 @@
 USE employees;
 
--- SELECT CONCAT(first_name, ' ', last_name)
--- AS 'first and last names' FROM employees
--- WHERE last_name LIKE 'E%'
---   AND last_name LIKE '%E';
+SELECT CONCAT(first_name, ' ', last_name)
+AS 'first and last names' FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 SELECT * FROM employees
 WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
@@ -17,3 +17,8 @@ AND MONTH (birth_date) = 12
 AND DAY (birth_date) = 25
 ORDER BY UNIX_TIMESTAMP (birth_date) ASC, UNIX_TIMESTAMP (hire_date) DESC
 LIMIT 1;
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'first and last name', datediff(CURDATE(), hire_date) AS 'Time working for the company' FROM employees
+WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
+  AND MONTH (birth_date) = 12
+  AND DAY (birth_date) = 25;
