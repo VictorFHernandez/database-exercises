a89c3f59567b2e40e19e100c1b32013ae2b393cb
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 09:55:26 -0500

    finished step 5 from FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,49 +1,19 @@
 USE employees;
---
--- SELECT * FROM employees
--- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
--- ORDER BY first_name;
---
--- SELECT * FROM employees
--- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
--- ORDER BY first_name DESC, last_name DESC;
---
--- SELECT * FROM employees
--- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
--- ORDER BY last_name, first_name;
---
--- SELECT * FROM employees
--- WHERE first_name = 'Irena'
---    OR first_name = 'Vidya'
---    OR first_name = 'Maya';
---
--- SELECT * FROM employees
--- WHERE gender = 'Male'
---     AND first_name = 'Irena'
---    OR first_name = 'Vidya'
---    OR first_name = 'Maya'
---
--- SELECT * FROM employees
--- WHERE last_name LIKE 'E%';
--- ORDER BY emp_no DESC;
---
--- SELECT * FROM employees
--- WHERE last_name LIKE 'E%'
---    OR last_name LIKE '%E';
 
 -- SELECT CONCAT(first_name, ' ', last_name)
 -- AS 'first and last names' FROM employees
 -- WHERE last_name LIKE 'E%'
 --   AND last_name LIKE '%E';
 
--- SELECT * FROM employees
--- WHERE last_name LIKE '%q%';
+SELECT * FROM employees
+WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
+  AND MONTH (birth_date) = 12
+  AND DAY (birth_date) = 25;
 
--- SELECT * FROM employees
--- WHERE last_name LIKE '%q%'
---   AND last_name NOT LIKE '%qu%';
 
 SELECT * FROM employees
 WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
 AND MONTH (birth_date) = 12
-AND DAY(birth_date) = 25;
+AND DAY (birth_date) = 25
+ORDER BY UNIX_TIMESTAMP (birth_date) ASC, UNIX_TIMESTAMP (hire_date) DESC
+LIMIT 1;
