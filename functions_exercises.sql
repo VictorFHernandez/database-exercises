e086f4728f6fd7178b2399b431bf45082fed24eb
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 10:19:11 -0500

    updated finished FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,7 +18,7 @@ AND DAY (birth_date) = 25
 ORDER BY UNIX_TIMESTAMP (birth_date) ASC, UNIX_TIMESTAMP (hire_date) DESC
 LIMIT 1;
 
-SELECT CONCAT(first_name, ' ', last_name) AS 'first and last name', datediff(CURDATE(), hire_date) AS 'Time working for the company' FROM employees
+SELECT CONCAT(first_name, ' ', last_name) AS 'first and last name', datediff(CURDATE(), hire_date) AS 'Days working for the company' FROM employees
 WHERE YEAR (hire_date) BETWEEN 1990 AND 1999
   AND MONTH (birth_date) = 12
   AND DAY (birth_date) = 25;
