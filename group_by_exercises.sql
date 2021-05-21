f0aaac310759da98602f7edb2fad4a046e276dee
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 11:56:41 -0500

    finished step 5 from GROUP BY exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,3 +12,6 @@ WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E'
 GROUP BY first_name, last_name ORDER BY first_name, last_name ASC;
 
+SELECT DISTINCT last_name FROM employees
+WHERE last_name Like '%q%'
+AND last_name NOT LIKE '%qu%';
