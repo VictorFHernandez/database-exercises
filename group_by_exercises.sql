fa09b5c0fb2395fb73350154a3c05558431a3a9e
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-21 11:46:43 -0500

    finished step 3 from GROUP BY exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,3 +1,8 @@
 USE employees;
 
 SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+GROUP BY last_name ORDER BY last_name ASC;
